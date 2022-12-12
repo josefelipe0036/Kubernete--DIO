6b9a56d7bab624afc2a93a06a0586afbb5ded3e5
José Felipe <75074939+josefelipe0036@users.noreply.github.com>
2022-12-12 12:17:54 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,9 +1,10 @@
-docker build -t ~/ESTUDOS/projeto kuber/k8s-projeto1-app-base:1.0 backend/.
-~/ESTUDOS/projeto kuber/k8s-projeto1-app-base:1.0 database/.
+docker build -t jose/projeto-backend:1.0 backend/.
+docker build -t jose/projeto-database:1.0 database/.
 
 
-docker push ~/ESTUDOS/projeto kuber/k8s-projeto1-app-base:1.0
-~/ESTUDOS/projeto kuber/k8s-projeto1-app-base:1.0
+
+docker build -t jose/projeto-backend:1.0
+docker build -t jose/projeto-database:1.0
 
 
 kubectl apply -f ./services.yml
